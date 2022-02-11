75c79f8faa133a0ebc0ea774a8a35fba45238f9d
Vitu <0vetor0@gmail.com>
2022-02-11 14:40:03 -0300

    update

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test', 'tes desc', 'https://www.teclasap.com.br/wp-content/uploads/2012/03/poop.jpg'),
+    new Recipe('test', 'tes desc', 'https://www.teclasap.com.br/wp-content/uploads/2012/03/poop.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
